f474f9b6ad06cd89064bd5792a1e0925d47a54b5
Kohei Matsumoto <matsumoto@ait.kyushu-u.ac.jp>
2026-09-11 13:28:04 +0900

    fix: script for experiment with NC-LQL

--- scripts/run_experiment_nc-lql.sh
+++ scripts/run_experiment_nc-lql.sh
@@ -1,5 +1,5 @@
-uv run run_nclql_socialnav.py --train.random-seed 17 --eval.render true --log.wandb true --log.save-model true
-uv run run_nclql_socialnav.py --train.random-seed 0 --eval.render true --log.wandb true --log.save-model true
-uv run run_nclql_socialnav.py --train.random-seed 5 --eval.render true --log.wandb true --log.save-model true
-uv run run_nclql_socialnav.py --train.random-seed 39 --eval.render true --log.wandb true --log.save-model true
-uv run run_nclql_socialnav.py --train.random-seed 105 --eval.render true --log.wandb true --log.save-model true
+uv run run_nclql_socialnav.py --train.random-seed 17 --eval.render  --log.wandb  --log.save-model 
+uv run run_nclql_socialnav.py --train.random-seed 0 --eval.render  --log.wandb  --log.save-model 
+uv run run_nclql_socialnav.py --train.random-seed 5 --eval.render  --log.wandb  --log.save-model 
+uv run run_nclql_socialnav.py --train.random-seed 39 --eval.render  --log.wandb  --log.save-model 
+uv run run_nclql_socialnav.py --train.random-seed 105 --eval.render  --log.wandb  --log.save-model 
