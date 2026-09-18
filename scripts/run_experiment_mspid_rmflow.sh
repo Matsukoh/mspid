adac3ca3bde302e4b9f9b0360981a1317d1153fb
Kohei Matsumoto <matsumoto@ait.kyushu-u.ac.jp>
2026-09-18 15:49:28 +0900

    add: scripts for experiment

---
+++ scripts/run_experiment_mspid_rmflow.sh
@@ -0,0 +1,5 @@
+uv run run_mspid_rmflow_socialnav.py --train.random-seed 17 --eval.render  --log.wandb  --log.save-model 
+uv run run_mspid_rmflow_socialnav.py --train.random-seed 0 --eval.render  --log.wandb  --log.save-model 
+uv run run_mspid_rmflow_socialnav.py --train.random-seed 5 --eval.render  --log.wandb  --log.save-model 
+uv run run_mspid_rmflow_socialnav.py --train.random-seed 39 --eval.render  --log.wandb  --log.save-model 
+uv run run_mspid_rmflow_socialnav.py --train.random-seed 105 --eval.render  --log.wandb  --log.save-model 
